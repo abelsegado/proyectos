b3dc4dbcd8fd3dd73c181555f3bf806f6fd7c82c
abelsegado <=>
2024-11-23 14:51:04 +0100

    tercer commit

--- devchallenges/qrcodegenerator/js/script.js
+++ devchallenges/qrcodegenerator/js/script.js
@@ -3,22 +3,20 @@ const $qr = document.querySelector('#qr-code')
 
 
 document.addEventListener('submit', (e) => {
-    console.log($qr);
     e.preventDefault()
     const qr = new QRCode($qr, input.value)
     input.value = ''
-    // display none main
     document.querySelector('main').style.display = 'none'
     document.querySelector('section').style.display = 'block'
 
 })
 
 
-// document.querySelector('#download').addEventListener('click', () => {
-//     // descargar imagen
-//     const link = document.createElement('a')
-//     link.download = 'qr-code.png'
-//     link.href = document.querySelector('#qr-code img').src
-//     link.click()
+document.querySelector('#download').addEventListener('click', () => {
+    // descargar imagen
+    const link = document.createElement('a')
+    link.download = 'qr-code.png'
+    link.href = document.querySelector('#qr-code img').src
+    link.click()
 
-// })
+})
